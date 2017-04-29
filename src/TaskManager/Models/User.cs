04b07f25d72aae83aea2bc593fce583e64e87a5d
Laura Clover <laura.clover3@gmail.com>
2017-04-28 22:48:08 -0600

    Added first five classes

--- src/TaskManager/Models/User.cs
+++ src/TaskManager/Models/User.cs
@@ -7,5 +7,13 @@ namespace TaskManager.Models
 {
     public class User
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public int UserID { get; set; }
+
+
     }
 }
